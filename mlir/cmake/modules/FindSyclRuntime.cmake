ec68963093bdc18e435bf0f66283b0b026d30f9c
Lee, Sang Ik <sang.ik.lee@intel.com>
2025-08-15 18:37:58 +0000

    FindSyclRuntime: Add support SyclRuntime_ROOT

--- mlir/cmake/modules/FindSyclRuntime.cmake
+++ mlir/cmake/modules/FindSyclRuntime.cmake
@@ -6,68 +6,62 @@
 #
 # If successful, the following variables will be defined:
 # SyclRuntime_FOUND
-# SyclRuntime_INCLUDE_DIRS
-# SyclRuntime_LIBRARY
-# SyclRuntime_LIBRARIES_DIR
 #
+
+if (TARGET SyclRuntime::SyclRuntime)
+    # If the target is already defined, we assume that the package has been found.
+    set(SyclRuntime_FOUND TRUE)
+    return()
+endif()
 
 include(FindPackageHandleStandardArgs)
 
-if(NOT DEFINED ENV{CMPLR_ROOT})
-    message(WARNING "Please make sure to install Intel DPC++ Compiler and run setvars.(sh/bat)")
-    message(WARNING "You can download standalone Intel DPC++ Compiler from https://www.intel.com/content/www/us/en/developer/articles/tool/oneapi-standalone-components.html#compilers")
-else()
+# if user provide CMake variable SyclRuntime_ROOT, that will have the highest priority for search.
+# if the environment variable CMPLR_ROOT is set, we will use it as a hint to find the SYCL runtime.
+if(DEFINED ENV{CMPLR_ROOT})
     get_filename_component(ONEAPI_VER "$ENV{CMPLR_ROOT}" NAME)
-    if(ONEAPI_VER VERSION_LESS 2024.0)
-        if(LINUX OR (${CMAKE_SYSTEM_NAME} MATCHES "Linux"))
-            set(SyclRuntime_ROOT "$ENV{CMPLR_ROOT}/linux")
-        elseif(WIN32)
-            set(SyclRuntime_ROOT "$ENV{CMPLR_ROOT}/windows")
-        endif()
+    # Need at least 2024.1 for experimental SPIR-V compiler support
+    if(ONEAPI_VER VERSION_LESS 2024.1)
+        # set hint to null path as we don't have a valid SYCL runtime with DPC++ compiler version < 2024.1
+        set(SyclRuntime_HINT "")
     else()
-        set(SyclRuntime_ROOT "$ENV{CMPLR_ROOT}")
+        set(SyclRuntime_HINT "$ENV{CMPLR_ROOT}")
     endif()
-    list(APPEND SyclRuntime_INCLUDE_DIRS "${SyclRuntime_ROOT}/include")
-    list(APPEND SyclRuntime_INCLUDE_DIRS "${SyclRuntime_ROOT}/include/sycl")
-
-    set(SyclRuntime_LIBRARY_DIR "${SyclRuntime_ROOT}/lib")
-
-    message(STATUS "SyclRuntime_LIBRARY_DIR: ${SyclRuntime_LIBRARY_DIR}")
-    find_library(SyclRuntime_LIBRARY
-        NAMES sycl
-        PATHS ${SyclRuntime_LIBRARY_DIR}
-        NO_DEFAULT_PATH
-        )
+else ()
+    # don't have a valid SYCL runtime, so we set the hint to an empty string
+    set(SyclRuntime_HINT "")
 endif()
+
+find_library(SyclRuntime_LIBRARY
+    NAMES sycl
+    HINTS ${SyclRuntime_HINT}
+    NO_CACHE
+    )
 
 if(SyclRuntime_LIBRARY)
     set(SyclRuntime_FOUND TRUE)
-    if(NOT TARGET SyclRuntime::SyclRuntime)
-        add_library(SyclRuntime::SyclRuntime INTERFACE IMPORTED)
-        set_target_properties(SyclRuntime::SyclRuntime
-            PROPERTIES INTERFACE_LINK_LIBRARIES "${SyclRuntime_LIBRARY}"
-      )
-      set_target_properties(SyclRuntime::SyclRuntime
-          PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${SyclRuntime_INCLUDE_DIRS}"
-      )
-    endif()
+    find_package_message(SyclRuntime "Found SyclRuntime: ${SyclRuntime_LIBRARY}" "")
+    add_library(SyclRuntime::SyclRuntime INTERFACE IMPORTED)
+    set_target_properties(SyclRuntime::SyclRuntime
+        PROPERTIES INTERFACE_LINK_LIBRARIES "${SyclRuntime_LIBRARY}"
+    )
+    cmake_path(GET SyclRuntime_LIBRARY PARENT_PATH SyclRuntime_LIBRARY_DIR)
+    cmake_path(GET SyclRuntime_LIBRARY_DIR PARENT_PATH SyclRuntime_BASE_DIR)
+
+    list(APPEND SyclRuntime_INCLUDE_DIRS "${SyclRuntime_BASE_DIR}/include")
+    list(APPEND SyclRuntime_INCLUDE_DIRS "${SyclRuntime_BASE_DIR}/include/sycl")
+
+    set_target_properties(SyclRuntime::SyclRuntime
+        PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${SyclRuntime_INCLUDE_DIRS}"
+    )
 else()
     set(SyclRuntime_FOUND FALSE)
+    find_package_message(SyclRuntime "Could not find SyclRuntime" "")
 endif()
 
 find_package_handle_standard_args(SyclRuntime
     REQUIRED_VARS
         SyclRuntime_FOUND
-        SyclRuntime_INCLUDE_DIRS
-        SyclRuntime_LIBRARY
-        SyclRuntime_LIBRARY_DIR
     HANDLE_COMPONENTS
 )
 
-mark_as_advanced(SyclRuntime_LIBRARY SyclRuntime_INCLUDE_DIRS)
-
-if(SyclRuntime_FOUND)
-    find_package_message(SyclRuntime "Found SyclRuntime: ${SyclRuntime_LIBRARY}" "")
-else()
-    find_package_message(SyclRuntime "Could not find SyclRuntime" "")
-endif()
