a70db86e7d05e801d253f10bae148c63f574b788
Lee, Sang Ik <sang.ik.lee@intel.com>
2026-06-02 20:39:09 +0000

    load_nd: handle special case of 8bit element load without vnni.

--- mlir/lib/Conversion/XeGPUToXeVM/XeGPUToXeVM.cpp
+++ mlir/lib/Conversion/XeGPUToXeVM/XeGPUToXeVM.cpp
@@ -431,13 +431,22 @@ class LoadStorePrefetchNdToXeVMPattern : public OpConversionPattern<OpType> {
           rewriter.eraseOp(op);
         } else {
           VectorType dstVecTy = cast<VectorType>(op.getValue().getType());
-          const bool vnni = op.getPacked().value_or(false);
+          bool vnni = op.getPacked().value_or(false);
           auto transposeValue = op.getTranspose();
           bool transpose =
               transposeValue.has_value() && transposeValue.value()[0] == 1;
+          // Handle tranpose request on small element size
           if (transpose && elemBitSize < 32) {
             tileW = tileW * elemBitSize / 32;
             elemBitSize = 32;
+          }
+          // Handle special case of plain 8bit element load
+          // with no vnni, no transpose, no vblocks
+          // Turn on vnni since non vnni load is not valid.
+          // vnni and non vnni have yields the same output so they can
+          // be interchanged.
+          if (elemBitSize == 8 && !vnni && !transpose) {
+            vnni = true;
           }
           VectorType loadedTy = encodeVectorTypeTo(
               dstVecTy, vnni ? rewriter.getI32Type()
